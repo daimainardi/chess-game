26b19f5f36670944173390867f41b6bb885e823e
Daiane Mainardi <daimainardi@hotmail.com>
2023-04-13 18:58:03 -0300

    Moving pieces

--- chess/src/chess/ChessMatch.java
+++ chess/src/chess/ChessMatch.java
@@ -1,6 +1,7 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
@@ -12,7 +13,6 @@ public class ChessMatch {
         board = new Board(8, 8);
         initialSetup();
     }
-
     public ChessPiece[][] getPieces() {
         ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
         for (int i = 0; i < board.getRows(); i++) {
@@ -22,11 +22,29 @@ public class ChessMatch {
         }
         return mat;
     }
+    public ChessPiece performChessMove (ChessPosition sourcePosition,ChessPosition targetPosition) {
+        Position source  = sourcePosition.toPosition();
+        Position target = targetPosition.toPosition();
+        ValidateSourcePosition(source);
+        Piece capturedPiece = makeMove(source, target);
+        return (ChessPiece) capturedPiece;
+    }
+
+    private Piece makeMove (Position source, Position target) {
+        Piece p = board.removePiece(source);
+        Piece capturedPiece = board.removePiece(target);
+        board.placePiece(p, target);
+        return capturedPiece;
+    }
+    private void ValidateSourcePosition (Position position) {
+        if (!board.thereIsAPiece(position)) {
+            throw new ChessException("There is no piece on source position");
+        }
+    }
 
     private void placeNewPiece(char column, int row, ChessPiece piece) {
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
     }
-
     private void initialSetup() {
         placeNewPiece('c', 1, new Rook(board, Color.WHITE));
         placeNewPiece('c', 2, new Rook(board, Color.WHITE));
@@ -42,5 +60,4 @@ public class ChessMatch {
         placeNewPiece('e', 8, new Rook(board, Color.BLACK));
         placeNewPiece('d', 8, new King(board, Color.BLACK));
     }
-
 }
